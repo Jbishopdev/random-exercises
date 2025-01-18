b5fa58efd4282bb0faaa3106520706ac3a8580fc
Jbishopdev <osrsironmangim@gmail.com>
2025-01-18 16:13:26 -0500

    Add event listeners

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const h3 = document.createElement("h3");
 const div = document.createElement("div");
 const h1 = document.createElement("h1");
 const p2 = document.createElement("p");
+const btn = document.querySelector("#btn");
+const btn2 = document.querySelector("#btn2");
 
 p.textContent = "Hey I'm Red!";
 h1.textContent = "I'm in a div!";
@@ -17,3 +19,6 @@ div.appendChild(p2);
 container.appendChild(p);
 container.appendChild(h3);
 container.appendChild(div);
+btn.addEventListener("click", function (e) {
+  e.target.style.background = "blue";
+});
