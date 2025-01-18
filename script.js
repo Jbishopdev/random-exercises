b527c24b09000df2d77691d8bb3c3740fdde12ee
Jbishopdev <osrsironmangim@gmail.com>
2025-01-18 15:32:54 -0500

    Add more styles via DOM manipulation

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const container = document.querySelector("#container");
-const pElement = document.createElement("p");
-pElement.textContent = "Hey I'm Red!";
-pElement.style.cssText = "color: red";
-container.appendChild(pElement);
+const p = document.createElement("p");
+const h3 = document.createElement("h3");
+const div = document.createElement("div");
+const h1 = document.createElement("h1");
+const p2 = document.createElement("p");
+
+p.textContent = "Hey I'm Red!";
+p.style.cssText = "color: red";
+h3.style.cssText = "color: blue";
+h3.textContent = "Hey I'm a blue H3!";
+div.style.backgroundColor = "pink";
+div.style.borderColor = "black";
+div.style.borderStyle = "solid";
+container.appendChild(p);
+container.appendChild(h3);
+container.appendChild(div);
